d310793d7707aee0bc452bdea63ee48e9a6caebc
Scott Davies <scott@scottdavies.uk>
2023-03-13 14:03:48 +0000

    finished mods 1-5

--- app/app.js
+++
@@ -1,6 +0,0 @@
-function startGame() {
-    //starting a new game
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to MultiMath! starting new game";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
